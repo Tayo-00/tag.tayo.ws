b50368b57685ecdb81f14e4606f4154b920b9a42
Lesley van der Lee <l.lee@tayo.ws>
2018-04-27 22:37:28 +0200

    APP: Add maps page with basic routing

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HomeModule } from './home/home.module';
+import { MapsModule } from './maps/maps.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgbModule.forRoot(),
+    LoadingBarRouterModule,
     BrowserModule,
     HomeModule,
+    MapsModule,
     AppRoutingModule
   ],
   providers: [],
