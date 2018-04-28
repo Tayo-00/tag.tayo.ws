7ec982134d51edbcf73a791965261767b7482272
Lesley van der Lee <l.lee@tayo.ws>
2018-04-28 11:30:09 +0200

    API: Update maps table

--- db.sql
+++ db.sql
@@ -1,11 +1,13 @@
 CREATE TABLE maps (
     id          serial,
-    mapset_id   integer,
-    map_id      integer,
-    players     integer,
-    combined_sr char,
-    player_sr   char,
-    title       char,
-    difficulty  char,
+    mapset_id   integer NOT NULL,
+    map_id      integer NOT NULL,
+    players     integer NOT NULL,
+    combined_sr char NOT NULL,
+    player_sr   char NOT NULL,
+    title       char NOT NULL,
+    mapper      char NOT NULL,
+    difficulty  char NOT NULL,
+    notelock    boolean,
     date_added  date
 );
